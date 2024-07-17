c02aac09af2115f2b93c6476a2256b5c871f89bd
arthurnov <art.dsky@gmail.com>
2024-07-17 20:49:45 +0300

    initial commit, basic functionality implemented

---
+++ script.js
@@ -0,0 +1,125 @@
+// connect to "output" elements
+const toDoBoard = document.getElementById("to-do-div");
+const inProgressBoard = document.getElementById("in-progress-div");
+const completedBoard = document.getElementById("completed-div");
+
+// if first run - create local storage items for later use
+if (JSON.parse(localStorage.getItem("to-do-list")) === null) {
+    localStorage.setItem("to-do-list", JSON.stringify(productList = []));
+};
+if (JSON.parse(localStorage.getItem("in-progress-list")) === null) {
+    localStorage.setItem("in-progress-list", JSON.stringify(productList = []));
+};
+if (JSON.parse(localStorage.getItem("completed-list")) === null) {
+    localStorage.setItem("completed-list", JSON.stringify(productList = []));
+};
+
+// draw all 3 sections of notes
+function buildBoard() {
+    drawSection("to-do");
+    drawSection("in-progress");
+    drawSection("completed");
+    clearForm();
+}
+
+function addNote(e) {
+    e.preventDefault();
+    let note = {};
+    for (const item of e.target) {
+        if (item.nodeName === "BUTTON") continue;
+        note[item.name] = item.value;
+    }
+    addNoteToList(note, "to-do");
+    drawSection("to-do");
+    clearForm();
+}
+
+function addNoteToList(note, list) {
+    const notes = JSON.parse(localStorage.getItem(`${list}-list`));
+    notes.push(note);
+    localStorage.setItem(`${list}-list`, JSON.stringify(notes));
+    drawSection(list);
+}
+
+function deleteNoteFromList(index, list) {
+    const notes = JSON.parse(localStorage.getItem(`${list}-list`));
+    notes.splice(index, 1);
+    localStorage.setItem(`${list}-list`, JSON.stringify(notes));
+    drawSection(list);
+}
+
+function drawSection(section) {
+    const list = JSON.parse(localStorage.getItem(`${section}-list`));
+    const nextList = (section === "to-do") ? "in-progress" : "completed";
+    const nextButtonText = (section === "to-do") ? "begin" : "done";
+    let notes = "";
+    for (let i = 0; i < list.length; i++) {
+        notes += `<div class="container note"><div class="note-content">`;
+        for (const key in list[i]) {
+            notes += `${key}: ${list[i][key]}<br>`;
+        }
+        const nextButtonButton = (section === "completed") ? "" : `<button onclick="move('${section}', ${i}, '${nextList}')">${nextButtonText}</button>`;
+        notes += `
+            </div>
+            <div class="note-actions">
+                <button onclick="deleteNoteFromList('${i}', '${section}')">delete</button>
+                ${nextButtonButton}
+            </div>
+        </div>`;
+    }
+
+    switch (section) {
+        case "to-do":
+            toDoBoard.innerHTML = notes;
+            break;
+        case "in-progress":
+            inProgressBoard.innerHTML = notes;
+            break;
+        case "completed":
+            completedBoard.innerHTML = notes;
+            break;
+        default:
+            toDoBoard.innerHTML = "error drawing notes on one of the boards";
+            break;
+    }
+}
+
+function move(source, index, target) {
+    const sourceList = JSON.parse(localStorage.getItem(`${source}-list`));
+    const targetList = JSON.parse(localStorage.getItem(`${target}-list`));
+
+    targetList.push(sourceList[index]);
+    sourceList.splice(index, 1);
+
+    localStorage.setItem(`${source}-list`, JSON.stringify(sourceList));
+    localStorage.setItem(`${target}-list`, JSON.stringify(targetList));
+
+    drawSection(source);
+    drawSection(target);
+
+}
+
+function clearForm() {
+    const allInputs = document.querySelectorAll('textarea, input');
+    allInputs.forEach(singleInput => singleInput.value = '');
+    allInputs[0].focus();
+}
+
+// ----- visual flare for "notes" when dragged -----
+
+// function handleDragStart(e) {
+//     this.style.opacity = '0.4';
+// }
+
+// function handleDragEnd(e) {
+//     this.style.opacity = '1';
+// }
+
+// function addEventsToNotes() {
+//     let items = document.querySelectorAll('.container .note');
+//     items.forEach(function (item) {
+//         item.addEventListener('dragstart', handleDragStart);
+//         item.addEventListener('dragend', handleDragEnd);
+//     });
+// }
+
